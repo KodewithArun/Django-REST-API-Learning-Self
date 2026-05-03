f7d2c3be55b635e83658c33aa7515edeb662edad
KodewithArun <contactwitharunlaudari@gmail.com>
2026-05-03 13:31:25 +0545

    Refactor: Upgrade to DRF Generic Views for Student CRUD API

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import StudentListCreateView, StudentDetailView
 
 urlpatterns = [
-    path('students/', StudentListCreateView.as_view(), name='student-list-create'),
-    path('students/<int:pk>/', StudentDetailView.as_view(), name='student-detail'),
+    path('students/', StudentListCreateView.as_view()),
+    path('students/<int:pk>/', StudentDetailView.as_view()),
 ]
